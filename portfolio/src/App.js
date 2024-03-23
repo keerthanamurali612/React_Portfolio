c3768a3464d1baad724126d70598dab2672243f6
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-03-23 09:38:35 +0530

    update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Project from './components/Project';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      {/* <Navbar/> */}
+      <About/>
+      {/* <Project/>
+      <Skills/>
+      <Testimonials/>
+      <Contact/> */}
+
+    </main>
   );
 }
 
